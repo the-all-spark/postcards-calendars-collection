ab7df746677c38273d610503bddc941aaee62657
Anna <anna.r.german1410@gmail.com>
2024-08-07 21:46:22 +0300

    feat: add auto reload on form pages

--- scripts/script_jQ_feedback.js
+++ scripts/script_jQ_feedback.js
@@ -69,6 +69,9 @@ $(function () {
             if(trueSend) {
                   $(".mainForm form").css("display","none");
                   $(".thanks").css("display","block");
+                  setTimeout(function () {
+                        location.reload();
+                  }, 3000);
             }
       
             return trueSend;
--- scripts/script_jQ_question.js
+++ scripts/script_jQ_question.js
@@ -1,27 +1,27 @@
 $(function () {
-      $("form").submit(function(e) {
+      $("form").submit(function (e) {
             e.preventDefault();           // предотвращение перезагрузки страницы (форма не отправляет данные самостоятельно)
-            
+
             let trueSend = true;          //флаг для отправки формы
             $("span").remove();           //убираем предупреждения при отправке формы
             $(".warning").remove();
 
             // проверка имени, поля "Задать вопрос"
-            $("#myname,textarea").each( function() {
+            $("#myname,textarea").each(function () {
                   const nameRegExp = /[а-яёa-z']+\s*/gi;
                   let entered = $(this).val();
                   //console.log(`Введено ${entered}`); 
 
-                  if($(this).val().length === 0 || !nameRegExp.test(entered) ) {
-                        $(this).css("border","3px solid rgba(167, 33, 0)");
+                  if ($(this).val().length === 0 || !nameRegExp.test(entered)) {
+                        $(this).css("border", "3px solid rgba(167, 33, 0)");
                         $(this).after("<p class='warning' style='color:rgba(167, 33, 0); margin-bottom: 0;'><b>Пожалуйста, корректно заполните поле</b></p>");
                         trueSend = false;
                         //console.log("НЕ ок!"); 
                   }
                   else {
-                        $(this).css("border","3px solid rgb(33, 148, 255)");
+                        $(this).css("border", "3px solid rgb(33, 148, 255)");
                         //console.log("ОК!");
-                  } 
+                  }
             });
 
             // Проверка e-mail
@@ -29,14 +29,14 @@ $(function () {
             let enteredMail = $("#myemail").val();
             //console.log(`Введено ${enteredMail}`); 
 
-            if($("#myemail").val().length === 0 || !mailRegExp.test(enteredMail) ) {
-                  $("#myemail").css("border","3px solid rgba(167, 33, 0)");
+            if ($("#myemail").val().length === 0 || !mailRegExp.test(enteredMail)) {
+                  $("#myemail").css("border", "3px solid rgba(167, 33, 0)");
                   $("#myemail").after("<span style='color:rgba(167, 33, 0)'><b>Пожалуйста, корректно заполните поле</b></span>");
                   trueSend = false;
                   //console.log("Почта НЕ ок!"); 
             }
             else {
-                  $("#myemail").css("border","3px solid rgb(33, 148, 255)");
+                  $("#myemail").css("border", "3px solid rgb(33, 148, 255)");
                   //console.log("Почта ОК!");
             }
 
@@ -45,23 +45,26 @@ $(function () {
             let enteredData = $("#dt").val();
             //console.log(`Введено ${enteredData}`); 
 
-            if($("#dt").val().length !== 0 && !dataRegExp.test(enteredData) ) {
-                  $("#dt").css("border","3px solid rgba(167, 33, 0)");
+            if ($("#dt").val().length !== 0 && !dataRegExp.test(enteredData)) {
+                  $("#dt").css("border", "3px solid rgba(167, 33, 0)");
                   $("#dt").after("<span style='color:rgba(167, 33, 0)'><b>Пожалуйста, корректно заполните дату</b></span>");
                   trueSend = false;
                   //console.log("Дата НЕ ок!"); 
             }
             else {
-                  $("#dt").css("border","3px solid rgb(33, 148, 255)");
+                  $("#dt").css("border", "3px solid rgb(33, 148, 255)");
                   //console.log("Дата ОК!");
             }
-            
+
             // если все поля заполнены верно
-            if(trueSend) {
-                  $(".mainForm form").css("display","none");
-                  $(".thanks").css("display","block");
+            if (trueSend) {
+                  $(".mainForm form").css("display", "none");
+                  $(".thanks").css("display", "block");
+                  setTimeout(function () {
+                        location.reload();
+                  }, 3000);
             }
-            
+
             return trueSend;
       });
 })
